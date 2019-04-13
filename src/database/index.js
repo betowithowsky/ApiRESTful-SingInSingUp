062101c1a3af73a6a1798d18e3454ba1672bcc36
= <betowithowsky@gmail.com>
2019-04-13 00:36:44 -0300

    conectando database com mlab

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/noderest', { useCreateIndex: true, useNewUrlParser: true });
+mongoose.connect('mongodb://adminapi:adminapi123@ds139576.mlab.com:39576/apirestfulsky', { useCreateIndex: true, useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
